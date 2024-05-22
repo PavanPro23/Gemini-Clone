478061560c384b01d62e7a137f04adc0103d84f7
Pavan <pinnintipavan04@gmail.com>
2024-05-22 10:15:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/sidebar'
+import Main from './components/Main/Main'
+
+const App = () => {
+  return (
+    <>
+      <Sidebar></Sidebar>
+      <Main></Main>
+    </>
+  )
+}
+
+export default App
